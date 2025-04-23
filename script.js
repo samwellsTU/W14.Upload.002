62f239b114b88711c854e0eacd00985ad1a79779
samwellsTU <111082260+samwellsTU@users.noreply.github.com>
2025-04-23 13:52:09 -0400

    4/23 inlcass

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-
-
-
 const ctx = new AudioContext();
 const gain = ctx.createGain();
-let audiobuffer= null;
+let audiobuffer = null;
 
-gain.connect(ctx.destination)
+gain.connect(ctx.destination);
 
+document
+  .getElementById("fileUpload")
+  .addEventListener("change", async (event) => {
+    let file = event.target.files[0];
+    let arraybuf = await file.arrayBuffer();
+    audiobuffer = await ctx.decodeAudioData(arraybuf);
+    let x = audiobuffer.getChannelData(0);
+    x.reverse();
+    audiobuffer.copyToChannel(x, 0);
+  });
 
+document.getElementById("play").addEventListener("click", () => {
+  if (audiobuffer) {
+    let sourceNode = ctx.createBufferSource();
+    sourceNode.buffer = audiobuffer;
+    sourceNode.connect(gain);
+    sourceNode.start();
+  }
+});
